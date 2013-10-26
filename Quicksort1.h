43a8187ad5724d8554f248b3b38585c48c1b8d0c
Zack Garza <dzackgarza@gmail.com>
2013-10-25 23:00:00 -0700

    dear lord it works

--- Quicksort1.h
+++ Quicksort1.h
@@ -9,26 +9,8 @@
 
 #ifndef QUICK_SORT_H
 #define QUICK_SORT_H
-#define ItemType int
+#define ItemType std::string
 #include <cstdlib>
-
-void Quicksort( ItemType*, size_t,
-                int (*)(const ItemType&, const ItemType&) );
-
-unsigned Partition(ItemType*, size_t,
-                   int (*)(const ItemType&, const ItemType&) );
-
-void SwapIndex(unsigned& i1, unsigned& i2);
-
-void SwapItem(ItemType*, unsigned&, unsigned&);
-
-void ptrswap(int** a, int** b);
-
-bool OutOfOrder(ItemType*, unsigned, unsigned);
-
-void MoveIndex(unsigned&, unsigned&);
-
-bool OutOfOrder (ItemType*, unsigned, unsigned,
-                  int (*fcmp)(const ItemType&, const ItemType&) );
-
+void Quicksort2( void* base, size_t nelem, size_t width,
+                int (*fcmp)( const void*, const void* ) );
 #endif
